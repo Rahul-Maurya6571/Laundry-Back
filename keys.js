cb831c9a997b1e058ffb5130cc0b8fbb724e0e07
Rahul Maurya <rahulmaurya6571@gmail.com>
2023-05-27 22:03:25 +0530

    backend file added

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONG_URI:"mongodb+srv://joshianuvrat:9557115385@cluster0.owy27ys.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"abjshdykn"
+
+}
---
+++ models/order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+// const User = mongoose.model("User")
+const {ObjectId} = mongoose.Schema.Types
+const orderSchema = mongoose.Schema({
+    product:{
+        type:String,
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    service:{
+        type:Array,
+        required:true
+    },
+    price:{
+        type:Number
+    },
+    createdBy:{
+        type:ObjectId,
+        ref:"User"
+    },
+    date:{
+        type: String
+    }
+
+})
+mongoose.model("CreateOrder",orderSchema)
